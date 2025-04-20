af42f6e9c737afcd1af415c94b130e3bd1ae177b
PRIYANSHU TIWARI <153541511+PRIYANSHU2026@users.noreply.github.com>
2025-04-20 16:57:00 +0530

    done

--- out/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"HNI1k5sGoOtLvJLyk8vXCWsKNwMBasj+YxgMSVSc/GE=\"\n}"
--- out/static/chunks/src_app_layout_tsx_c0237562._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__9a168d68._.css",
-  "static/chunks/src_e12b9262._.js",
-  "static/chunks/node_modules_aad83e88._.js"
-],
-    source: "dynamic"
-});
